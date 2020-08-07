f8c59de1bedd17bb0e279eae267d32704c13af28
Nikolay from Orsk <dimitrievk56@mail.ru>
2020-08-07 19:42:55 +0500

    Реализован аккордион

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import togglePopUp from './modules/togglePopUp';
 import addBlocks from './modules/addBlocks';
+import accordion from './modules/accordion';
 
 
 togglePopUp();
 addBlocks();
+accordion();
